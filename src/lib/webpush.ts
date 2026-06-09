9ad90c0879ce08abda8cb6f78b48ed842f8dda24
NgoGiaLam1 <lamngo14857@gmail.com>
2026-06-09 20:33:17 +0700

    update thông báo

--- src/lib/webpush.ts
+++ src/lib/webpush.ts
@@ -1,23 +1,10 @@
 import webpush from "web-push";
 
-console.log({
-
- email:
- process.env.VAPID_EMAIL,
-
- public:
- process.env.VAPID_PUBLIC_KEY,
-
- private:
- process.env.VAPID_PRIVATE_KEY
-
-});
-
 webpush.setVapidDetails(
 
  process.env.VAPID_EMAIL!,
 
- process.env.VAPID_PUBLIC_KEY!,
+ process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!,
 
  process.env.VAPID_PRIVATE_KEY!
 
